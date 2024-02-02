e2f19bb1d1748eefef19ee02eb545b5b32fb2fdd
Cassio Maciel Lemos <cassio.lemos@petrobras.com.br>
2024-02-01 22:47:29 -0300

    Fiz o Logging funcionar na subfunção Limpei o cache da forma correta requests_cache.clear() Modifiquei a forma de ignorar o FutureWarning

--- AtualizarDados.py
+++ AtualizarDados.py
@@ -1,28 +1,41 @@
-import fundamentus
+import logging
+import requests_cache
 import warnings
-import os
 import pandas as pd
 import datetime
+logging.basicConfig(level=logging.CRITICAL)
+import fundamentus
+
+logger_main = logging.getLogger("minha_funcao_main")
+logger_main.setLevel(logging.INFO)
+
+requests_cache.install_cache("http_cache")
+requests_cache.clear()
 
 dados_atuais_path = './.BancoDados/RelatoriosContabeis'
 dados_historicos_path = './.BancoDados/historicas'
 
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
 get_resultado = fundamentus.get_resultado()
+get_resultado = get_resultado.sort_values(by='liq2m', ascending=False)
+get_resultado = get_resultado[get_resultado['liq2m'] > 10]
 
 index = 0
 resultados = pd.DataFrame()
 
 for acao in get_resultado.index:
-    print(acao)
-    if os.path.exists("http_cache.sqlite"):
-        os.remove("http_cache.sqlite")
-    with warnings.catch_warnings():
+    logger_main.info(acao)
+    # if os.path.exists("http_cache.sqlite"):
+    #     os.remove("http_cache.sqlite")
+    # with warnings.catch_warnings():
         # https://docs.python.org/3/library/warnings.html
-        warnings.simplefilter("ignore")
-        resultados_acao = fundamentus.get_papel(acao)
-        resultados = resultados._append(resultados_acao)
+        # warnings.simplefilter("ignore")
+    resultados_acao = fundamentus.get_papel(acao)
+    # resultados = resultados._append(resultados_acao)
+    resultados = pd.concat([resultados, resultados_acao])
     index += index
-    print(acao)
+    logger_main.debug('saida', acao)
 
 resultados = resultados.set_index('Papel')
 resultados.to_csv(dados_atuais_path)
